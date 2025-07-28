d12dd2f3c1e9bdaea4f665a0b20811abe4d872ff
momin-riyadh <mominriyadh@gmail.com>
2025-07-28 12:31:37 +0600

    fix and deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {LOGO} from './assets/images/logo.svg'
+import LOGO from './assets/images/logo.svg'
 import './App.css'
 
 function App() {
@@ -7,23 +7,36 @@ function App() {
     return (
         <>
             <div className="h-screen w-full flex flex-col bg-[#f4f7fd]">
+
                 {/* Top Navigation Bar */}
                 <div className="h-20 bg-white fixed top-0 left-0 right-0 z-50">
-                    <a href="">
-                        <img src={LOGO} alt="Logo" className="h-16 mx-auto mt-2"/>
-                    </a>
+
+                    <div className="px-4 py-2 flex justify-between items-center">
+                        <a href="" className="w-70">
+                            <img width={120} src={LOGO} alt="Logo" className="h-16"/>
+                        </a>
+                        <button className=" bg-[#635FC7] text-white px-4 py-2 rounded-3xl"> + Add New Task </button>
+                    </div>
+
                 </div>
 
                 <div className="flex flex-1">
                     {/* Sidebar */}
-                    <div className="w-72 bg-white h-full">
+                    <div className="w-72 bg-white h-full border-r-1 border-[#E4EBFA] pt-20">
+                        <div className="p-4">
+                            <h2 className="text-[12px] font-semibold mb-4 text-[#828FA3] uppercase">All Boards (3)</h2>
+                            <ul className="space-y-3">
+                                <li><a href="#" className="text-[#635FC7] hover:underline">Home</a></li>
+                                <li><a href="#" className="text-gray-600 hover:underline">Tasks</a></li>
+                                <li><a href="#" className="text-gray-600 hover:underline">Projects</a></li>
+                                <li><a href="#" className="text-gray-600 hover:underline">Settings</a></li>
+                            </ul>
+                        </div>
                     </div>
 
                     {/* Main Content */}
                     <div className="flex-1 p-4 mt-22">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi autem corporis eaque
-                        eligendi id ipsam maiores, modi rerum veritatis? Dolores fuga laudantium neque quisquam sit.
-                        Culpa ipsam iure neque.
+
                     </div>
                 </div>
             </div>
