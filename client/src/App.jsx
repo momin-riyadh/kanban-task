9093db54c8f866dbb04984f65a8cd8b2d24e9b6e
momin-riyadh <mominriyadh@gmail.com>
2025-07-28 14:46:01 +0600

    fix and deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,20 +33,20 @@ function App() {
                     <div className="w-72 bg-white h-full border-r-1 border-[#E4EBFA] pt-20">
                         <div className="p-4">
                             <h2 className="text-[12px] font-semibold mb-4 text-[#828FA3] uppercase">All Boards (3)</h2>
-                            <ul className="space-y-2 -ml-4">
-                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl">
+                            <ul className="space-y-2 ">
+                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl -ml-4 pl-2">
                                     <a href="#"
                                        className="text-[#635FC7] hover:text-white active:bg-[#635FC7] active:text-white block p-2">Home</a>
                                 </li>
-                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl">
+                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl -ml-4 pl-2">
                                     <a href="#"
                                        className="text-gray-600 hover:text-white active:bg-[#635FC7] active:text-white block p-2">Tasks</a>
                                 </li>
-                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl">
+                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl -ml-4 pl-2">
                                     <a href="#"
                                        className="text-gray-600 hover:text-white active:bg-[#635FC7] active:text-white block p-2">Projects</a>
                                 </li>
-                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl">
+                                <li className="hover:bg-[#635FC7] hover:text-white rounded-r-3xl -ml-4 pl-2">
                                     <a href="#"
                                        className="text-gray-600 hover:text-white active:bg-[#635FC7] active:text-white block p-2">Settings</a>
                                 </li>
