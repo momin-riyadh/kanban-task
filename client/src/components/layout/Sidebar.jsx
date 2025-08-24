1174b678a33111391e0fdaa5749869a4279316ed
momin-riyadh <mominriyadh@gmail.com>
2025-08-24 11:23:59 +0600

    fix and deploy

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,6 +1,12 @@
 import iconBoard from '../../assets/images/icon-board.svg';
+import Moon from '../../assets/images/moon.svg';
+import Sun from '../../assets/images/sun.svg';
+import {useState} from 'react';
+import IconShow from '../../assets/images/icon-show-sidebar.svg';
+import IconHide from '../../assets/images/icon-hide-sidebar.svg';
 
 function Sidebar() {
+    const [isDark, setIsDark] = useState(false);
     const menuItems = [
         {name: 'Home', href: '#', isActive: true, icon: iconBoard},
         {name: 'Tasks', href: '#', isActive: false, icon: iconBoard},
@@ -31,6 +37,26 @@ function Sidebar() {
                     ))}
                 </ul>
             </div>
+
+
+            {/*Sidebar Footer*/}
+            <div className="flex flex-col items-center mt-auto p-4">
+                <div className="flex items-center gap-2">
+                    <img src={Sun} alt=""/>
+                    <label className="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" className="sr-only peer" onChange={e => setIsDark(e.target.checked)}/>
+                        <div
+                            className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#635FC7]"></div>
+                    </label>
+                    <img src={Moon} alt=""/>
+                </div>
+
+                <div className="flex items-center gap-2 mt-4">
+                    <img src={IconHide} alt=""/> Hide Sidebar
+                </div>
+            </div>
+            {/*End Sidebar Footer*/}
+
         </div>
     );
 }
