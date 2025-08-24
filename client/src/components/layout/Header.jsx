84991782ab89e08233bd20948976db2694d902ce
momin-riyadh <mominriyadh@gmail.com>
2025-08-24 12:34:52 +0600

    fix and deploy

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                     <img width={120} src={LOGO} alt="Logo" className="h-16"/>
                 </a>
                 <div className="flex items-center gap-2">
-                    <button className="bg-[#635FC7] text-white px-4 py-2 rounded-3xl">
+                    <button className="bg-[#635FC7] text-white px-4 py-3 rounded-3xl">
                         <i className="bi bi-plus"></i> Add New Task
                     </button>
                     <i className="bi bi-three-dots-vertical text-[#828FA3] text-[20px] cursor-pointer"></i>
